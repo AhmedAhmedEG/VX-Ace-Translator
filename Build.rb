f77f015204400e6d3e55e344157d17f2d35a0145
AhmedAhmedEG <ahmed.ahmed.a.m.aa12@gmail.com>
2023-11-17 04:06:32 +0200

    Update Build.rb

--- Build.rb
+++ Build.rb
@@ -5,5 +5,5 @@ Dir.chdir(File.dirname(__FILE__))
 Dir.mkdir('Build') unless Dir.exist?('Build')
 FileUtils.cp_r('Resources', File.join('Build/Resources'))
 
-system("Build.bat")
+system("Builder.bat")
 
