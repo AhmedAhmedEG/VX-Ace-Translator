168196cd648db5c28b5b7f15a209d8da63ba5299
AhmedAhmedEG <ahmed.ahmed.a.m.aa12@gmail.com>
2023-11-22 00:20:16 +0200

    Delete Builder.rb

--- Builder.rb
+++
@@ -1,9 +0,0 @@
-require 'fileutils'
-
-Dir.chdir(File.dirname(__FILE__))
-
-Dir.mkdir('Build') unless Dir.exist?('Build')
-FileUtils.cp_r('Resources', File.join('Build/Resources'))
-
-system("Build.bat")
-
